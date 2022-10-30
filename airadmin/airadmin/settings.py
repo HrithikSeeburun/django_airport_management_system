2b24841d30225e53451c843acd45ee603b24b5d5
Hrithik Seeburun <117035713+HrithikSeeburun@users.noreply.github.com>
2022-10-31 00:28:08 +0530

    Update settings.py

--- airadmin/airadmin/settings.py
+++ airadmin/airadmin/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a9axb!&g&#t#v$z^@0mub@l$$5m%u&-@i%aimtgew(!=z-$nmd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
